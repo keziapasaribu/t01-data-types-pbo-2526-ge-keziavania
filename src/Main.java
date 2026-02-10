c8f7406206f2eef2d4d01576a6fd325d8ddf44ff
keziapasaribu <kanayamijelia@gmail.com>
2026-02-10 09:58:51 +0700

    T01

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: implement solution
-    }
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int hasil = a + b;
+
+        if (a > 0 && b > 0 && hasil < 0) {
+            System.out.println("OVERFLOW");
+        } else if (a < 0 && b < 0 && hasil >= 0) {
+            System.out.println("OVERFLOW");
+        } else {
+            System.out.println(hasil);
+        }
+
+        sc.close();
+     }
 }
+
+// soal 2
+double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        // 2. Jumlahkan menggunakan float
+        float hasilFloat = (float) x + (float) y;
+
+        // 3. Jumlahkan menggunakan double
+        double hasilDouble = x + y;
+
+        // 4. Hitung selisih absolut (selalu positif)
+        double selisih = Math.abs(hasilFloat - hasilDouble);
+
+        // 5. Tampilkan hasil dengan 6 digit di belakang koma
+        System.out.printf("%.6f\n", selisih);
+
+        sc.close();
